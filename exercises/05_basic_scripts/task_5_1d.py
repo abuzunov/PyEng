68616ebd9121c8f5dc703f8ab9975734c68ba191
abuzunov <alexander.buzunov@gmail.com>
2021-11-15 12:56:32 -0600

    5.2

--- exercises/05_basic_scripts/task_5_1d.py
+++ exercises/05_basic_scripts/task_5_1d.py
@@ -43,3 +43,10 @@ london_co = {
         "routing": True,
     },
 }
+
+device = input("Введите имя устройства: ")
+keys = ', '.join(london_co[device].keys())
+param = input(f"Введите имя параметра ({keys}): ")
+param = param.lower()
+print(london_co[device].get(param, "Такого параметра нет"))
+
